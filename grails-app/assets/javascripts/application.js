cf1f3d1968c04dd6379cda1cfbafeac5a365bcc3
Jom Cordova <jmccordova@gmail.com>
2021-04-30 14:05:25 +0800

    Added Datatables and Select 2

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -8,4 +8,71 @@
 //= require jquery-3.3.1.min
 //= require bootstrap
 //= require popper.min
+//= require select2.min
+//= require jquery.dataTables.min
+//= require dataTables.buttons.min
+//= require jszip.min
+//= require pdfmake.min
+//= require vfs_fonts
+//= require buttons.html5.min
 //= require_self
+
+if (typeof jQuery !== 'undefined') {
+    (function($) {
+        $(document).ajaxStart(function() {
+            $('#spinner').fadeIn();
+        }).ajaxStop(function() {
+            $('#spinner').fadeOut();
+        });
+    })(jQuery);
+}
+
+var dataTable, exportTable
+$(document).ready( function () {
+	$("#spinner").fadeOut("fast");
+
+	dataTable = $(".data-table").DataTable({
+        responsive: true,
+        scrollX: true
+    });
+
+    exportTable = $(".export-table").DataTable({
+        responsive: true,
+        scrollX: true,
+        dom: 'Blfrtip',
+        buttons: [{
+            extend: 'copyHtml5',
+            exportOptions: {
+                columns: ":not(.not-for-export)"
+            }
+        }, {
+            extend: 'csvHtml5',
+            exportOptions: {
+                columns: ":not(.not-for-export)"
+            }
+        }, {
+            extend: 'excelHtml5',
+            exportOptions: {
+                columns: ":not(.not-for-export)"
+            }
+        }, {
+            extend: 'pdfHtml5',
+            orientation: 'landscape',
+            pageSize: 'A4',
+            exportOptions: {
+                columns: ":not(.not-for-export)"
+            }
+        }]
+    });
+
+    $(".select2").select2();
+}).on("ajaxSend submit", function() {
+    $("input[type='submit']").blur();
+    $("#spinner").fadeIn('fast');
+})
+.on("ajaxStop", function() {
+    $("#spinner").fadeOut('fast');
+})
+.on("ajaxError", function(event, jqxhr, settings, exception) {
+    $("#spinner").hide();                
+});
